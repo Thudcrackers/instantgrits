af56fe78ed331c2d485c9798895878c21615e58a
Jack Morton <jmort87@gmail.com>
2018-11-27 09:57:08 -0500

    Stuff

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,42 +1,58 @@
 class PostsController < ApplicationController
 
+before_action :set_post, only: [:show, :edit, :update, :destroy]
+before_action :authenticate_user!
+
 def index
     @posts = Post.all
 end
 
 def new
     @post = Post.new
-    
 end
 
 def create
-    @post = Post.create(post_params)
-    redirect_to posts_path
+    if @post = Post.create(post_params)
+        flash[:success] = "Your post has been created!"
+        redirect_to posts_path
+    else
+        flash.now[:alert] = "Your new post couldn't be created!  Please check the form."
+        render :new
+    end
 end
 
 def show
-    @post = Post.find(params[:id])
 end
 
 def edit
-    @post = Post.find(params[:id])
 end
 
 def update
-    @post = Post.find(params[:id])
-    @post.update(post_params)
-    redirect_to(post_path(@post))
+    if @post.update(post_params)
+        flash[:success] = "Post updated."
+        redirect_to(post_path(@post))
+    else
+        flash.now[:alert] = "Update failed.  Please check the form."
+        render :edit
+    end
 end
 
 def destroy
-    @post = Post.find(params[:id])
-    @post.destroy
-    redirect_to posts_path
+    if @post.destroy
+        flash[:success] = "Post deleted."
+    else
+        flash[:alert] = "Something happened."
+        redirect_to root_path
+    end
 end
 
 private
 def post_params
     params.require(:post).permit(:image, :caption)
+end
+
+def set_post
+    @post = Post.find(params[:id])
 end
 
 end
